e4259ea9f6257dfed68fe514dc6a793859e6ee90
EmelieAxi@hotmail.com <emelieaxi@hotmail.com>
2025-10-29 12:35:50 +0100

    Lagt till Rubrik och Content i About

--- RestaurangReact/src/components/About.jsx
+++ RestaurangReact/src/components/About.jsx
@@ -1,8 +1,20 @@
+import Button from "./Button";
+import PageCaption from "./PageCaption";
+import PageTitle from "./PageTitle";
+
 export default function About() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Welcome to our restaurant! We are dedicated to providing the best dining experience.</p>
-    </div>
+    <section className="about">
+        <img src="src/assets/food-3581341_1280.jpg" alt="Sushi" id="sushi-img" />
+        <div className="Text">
+        <PageTitle title="En plats för sushiälskare – och nyfikna" />
+        <PageCaption caption="Hos oss handlar sushi om mer än bara mat – det handlar om upplevelsen. Vi rullar varje bit med omsorg, serverar den med ett leende och bjuder in dig att upptäcka nya smaker, dofter och kombinationer. Oavsett om du är sushi-proffs eller bara nyfiken på något nytt, finns alltid något att älska här." />
+        <div className="button-container">
+            <Button text="Lär dig mer" />
+            <Button text="Titta på Video" type="secondary" />
+        </div>
+    
+        </div>
+    </section>
   );
 }
